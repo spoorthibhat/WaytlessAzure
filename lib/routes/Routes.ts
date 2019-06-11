888be5c3930a877bb4a7776f9ec8f0744a93eac1
Spoorthi Bhat <spoorthicbhat@gmail.com>
2019-06-10 18:37:41 -0700

    Fixing logout session clearing

--- lib/routes/Routes.ts
+++ lib/routes/Routes.ts
@@ -58,10 +58,13 @@ export class Routes {
     );
 
     app.get('/logout', function(req, res){
+        console.log("Inside log out route");
         req.logout();
         delete req.user;
         req.session.destroy(function (err) {
-            res.redirect('/'); 
+            console.log("Inside session destroy");
+            res.clearCookie('connect.sid');
+            res.redirect('/#/home'); 
           });
       });
 
